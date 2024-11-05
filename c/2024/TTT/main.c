4f9bb59301fd3c6018e5951059df9f693d1b1517
marko101010 <marko.dogas@noseryoung.ch>
2024-11-05 10:37:03 +0100

    updates od Tic Tac Toe Game

--- c/2024/TTT/main.c
+++ c/2024/TTT/main.c
@@ -2,29 +2,39 @@
 
 int field [3][3] = {{0}};
 
+void printField();
+
+void setField();
+
 int main() {
     printf("Welcome to the Tic Tac Toe Game!!!!!\n");
 
 
     while(1) {
-        //printfield
+        printField();
+        setField();
+    }
+    return 0;
+}
+    void printField() {
         for (int j=0; j<3 ; j++) {
             for (int i=0; i<3 ; i++) {
                 printf("%d  ", field [j][i]);
             }
             printf("\n");
         }
-
-        //setField
-        printf("Choose an x coordinate \n");
-        int x = -1;
-        scanf(" %d", &x);
-
-        printf("Choose an y coordinate \n");
-        int y = -1;
-        scanf(" %d", &y);
-
-        field [x] [y] = 1;
     }
-    return 0;
-}
+        void setField() {
+            printf("Choose an x coordinate \n");
+            int x = -1;
+            scanf(" %d", &x);
+
+            printf("Choose an y coordinate \n");
+            int y = -1;
+            scanf(" %d", &y);
+
+            field [x] [y] = 1;
+        }
+
+
+
